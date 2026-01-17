57c720ea95db87c0b218df8fcd884196d38b2b30
Ayumu Ono <ayumuco0407@gmail.com>
2026-01-17 17:28:03 +0900

    017

--- module/can_rx/mod_can_rx.c
+++ module/can_rx/mod_can_rx.c
@@ -17,7 +17,9 @@ static inline bool check_transfer_id(uint8_t in_ch_id, CanMessage *in_can_msg_pt
     bool rtn = false;
     if(in_ch_id >= TX_CHANNEL_NUM || in_can_msg_ptr->m_msg_id >= 2048){rc = -1;}
     if(!rc){
-        rtn = (g_transfer_st[in_can_msg_ptr->m_msg_id].m_transfer_ch_flg) & ID2FLAG(in_ch_id);
+        if((g_transfer_st[in_can_msg_ptr->m_msg_id].m_transfer_ch_flg) & ID2FLAG(in_ch_id)) {
+            rtn = true;
+        }
     }
     if(out_rc_ptr) *out_rc_ptr = rc;
     return rtn;
@@ -26,9 +28,9 @@ static inline bool check_transfer_id(uint8_t in_ch_id, CanMessage *in_can_msg_pt
 int mod_can_rx_init(void){
     int rc = 0;
     for(int i=0; i < 2048; i++){
-        g_transfer_st[i].m_transfer_ch_flg 
-            = (ID2FLAG(TX_CHANNEL_0)|ID2FLAG(TX_CHANNEL_1)|ID2FLAG(TX_CHANNEL_2)|ID2FLAG(TX_CHANNEL_3));
+        g_transfer_st[i].m_transfer_ch_flg = 0;
     }
+    g_transfer_st[0x100].m_transfer_ch_flg  = (ID2FLAG(TX_CHANNEL_0)|ID2FLAG(TX_CHANNEL_2)|ID2FLAG(TX_CHANNEL_3));
 
     rc = lib_queue_init(&g_mod_if_can_rx_q);
     return rc;
@@ -38,19 +40,20 @@ int mod_can_rx_main_process(void){
     int rc = 0;
 
     CanMessage *can_msg_ptr = NULL;
-    Node* node_ptr = NULL;
+
 
     /* Receive messages from all channels and push them into the queue */
     for(int i=0; rc == 0 && i < TX_CHANNEL_NUM; i++){
         can_msg_ptr = mod_can_channel_rcv_msg(i, &rc);
-        while(!rc && can_msg_ptr==NULL){
+        while(!rc && can_msg_ptr!=NULL){
             rc = lib_queue_push(&g_mod_if_can_rx_q, &can_msg_ptr->m_node);
             if(!rc) can_msg_ptr = mod_can_channel_rcv_msg(i, &rc);
         }
     }
     
     /* Process messages in the queue and forward them to appropriate channels */
-    node_ptr = lib_queue_pop(&g_mod_if_can_rx_q,&rc);
+    if(!rc) {
+        Node* node_ptr = lib_queue_pop(&g_mod_if_can_rx_q,&rc);
     while(!rc && node_ptr != NULL){
         can_msg_ptr = node_ptr->m_body_ptr;
         if(can_msg_ptr == NULL) rc = -1;    
@@ -58,13 +61,14 @@ int mod_can_rx_main_process(void){
         for(int i = 0; !rc && i < TX_CHANNEL_NUM; i++){
             if(check_transfer_id(i, can_msg_ptr, &rc) && !rc){
                 SharedCanMessage* shared_can_msg_ptr = mod_can_message_copy(can_msg_ptr, &rc);
-                if(!shared_can_msg_ptr && !rc){
+                    if(!rc && shared_can_msg_ptr != NULL){
                     rc = mod_can_tx_send_msg(i, shared_can_msg_ptr);
                 }
             }
         }
         if(!rc) rc = mod_can_message_release(can_msg_ptr);
         if(!rc) node_ptr = lib_queue_pop(&g_mod_if_can_rx_q,&rc);
+    }
     }
 
     return rc;
